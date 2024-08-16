e006d2dd15e9d66b19f4d9a4f38de5d99fef3130
Arnaud Peralta <arnaud.peralta@gmail.com>
2024-08-17 01:46:03 +0200

    can hit on count gates

--- scenes/level/CountGate.cs
+++ scenes/level/CountGate.cs
@@ -8,35 +8,55 @@ public partial class CountGate : Node3D
 
     private uint choiceAValue;
     private uint choiceBValue;
+    private Label3D choiceALabel;
+    private Label3D choiceBLabel;
 
     public override void _Ready()
     {
-        var choiceA = GetNode<Label3D>("ChoiceA/Area3D/Label3D");
-        var choiceB = GetNode<Label3D>("ChoiceB/Area3D/Label3D");
+        choiceALabel = GetNode<Label3D>("ChoiceA/Area3D/Label3D");
+        choiceBLabel = GetNode<Label3D>("ChoiceB/Area3D/Label3D");
         choiceAValue = GD.Randi() % 10;
         choiceBValue = GD.Randi() % 10;
         if (GD.Randi() % 2 == 0)
         {
             Rotate(new Vector3(0, 1, 0), Mathf.DegToRad(180));
-            choiceA.Rotate(new Vector3(0, 1, 0), Mathf.DegToRad(180));
-            choiceB.Rotate(new Vector3(0, 1, 0), Mathf.DegToRad(180));
-            choiceA.Text = $"-{choiceAValue}";
-            choiceB.Text = $"+{choiceBValue}";
+            choiceALabel.Rotate(new Vector3(0, 1, 0), Mathf.DegToRad(180));
+            choiceBLabel.Rotate(new Vector3(0, 1, 0), Mathf.DegToRad(180));
+            choiceALabel.Text = $"-{choiceAValue}";
+            choiceBLabel.Text = $"+{choiceBValue}";
         }
-        choiceA.Text = $"+{choiceAValue}";
-        choiceB.Text = $"-{choiceBValue}";
+        choiceALabel.Text = $"+{choiceAValue}";
+        choiceBLabel.Text = $"-{choiceBValue}";
     }
 
     private void OnPositiveGateBodyEntered(StaticBody3D body)
     {
-        EmitSignal(SignalName.CountGateEntered, choiceAValue);
-        QueueFree();
+        if (body.CollisionLayer == 1)
+        {
+            EmitSignal(SignalName.CountGateEntered, choiceAValue);
+            QueueFree();
+        }
+        else
+        {
+            choiceAValue++;
+            choiceALabel.Text = $"+{choiceAValue}";
+            body.GetParent().QueueFree();
+        }
     }
 
     private void OnNegativeGateBodyEntered(StaticBody3D body)
     {
-        EmitSignal(SignalName.CountGateEntered, -choiceBValue);
-        QueueFree();
+        if (body.CollisionLayer == 1)
+        {
+            EmitSignal(SignalName.CountGateEntered, -choiceBValue);
+            QueueFree();
+        }
+        else
+        {
+            choiceBValue++;
+            choiceBLabel.Text = $"-{choiceBValue}";
+            body.GetParent().QueueFree();
+        }
     }
 
 }
